114b0257014211e82f71b21a9ddb04c2cfacc007
Tyler Bond <>
2021-05-23 00:51:32 +0000

    TODOS 1.2

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = '';
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,20 +15,25 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question('Your name: ');
+candidateName = input.question('Please enter your Name: ');
+
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log(question);
+candidateAnswer = input.question("Enter your answer: ");
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
+if(candidateAnswer.includes('Sally Ride')){
+  console.log('Correct');
+} else{
+  console.log('Incorrect');
+}
   let grade;
   
 
@@ -38,7 +43,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log(candidateName);
+  console.log(`Welcome, ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
