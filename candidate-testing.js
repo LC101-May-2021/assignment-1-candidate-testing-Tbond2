58533be720e2ad47be78646cc2ed348a31b2d414
Tyler Bond <>
2021-06-04 01:59:26 +0000

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ["Who was the first American woman in space?","True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', ;;'Trajectory', 45] what entry is at index 2?","What is the minimum crew size for the International Space Station (ISS)?"]
+let correctAnswer = ['Sally Ride', "True", "40", "Trajectory", "3"]
+let candidateAnswer = '';
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,30 +15,33 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question('Please enter your Name: ');
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+ console.log(question[0]);
+ candidateAnswer = input.question("")
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
+if(candidateAnswer.includes(correctAnswer[0])){
+  console.log(`Your answer, ${candidateAnswer}, is Correct`);
+} else{
+  console.log(`Your answer, ${candidateAnswer}, is Incorrect`);
+}
+  let grade
   
 
-  return grade;
+  return grade
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Welcome, ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
