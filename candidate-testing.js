694627201ad21d89a341dd634ab81417aaffddb6
Tyler Bond <>
2021-06-05 02:41:50 +0000

    Cleaned up code

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,13 +5,13 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 5
-let correctAnswer = 0
+let question = "Who was the first American woman in space?"
+let correctAnswer = 'Sally Ride'
 let candidateAnswer = '';
 let questions = ["Who was the first American woman in space?","True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit','Trajectory', 45] what entry is at index 2?","What is the minimum crew size for the International Space Station (ISS)?"]
 let correctAnswers = ['Sally Ride', "True", "40", "Trajectory", "3"]
 let candidateAnswers = []
-let score = 0
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -21,29 +21,28 @@ candidateName = input.question('Please enter your Name: ');
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for(let i = 0; i < questions.length; i++){
-  candidateAnswers[i] = input.question(`${i+1}) ${questions[i]} \n Your Answer: `) 
+  candidateAnswer = input.question(`${i+1}) ${questions[i]} \n Your Answer: `) 
   console.log(`Correct Answer: ${correctAnswers[i]}`)
+  candidateAnswers.push(candidateAnswer)
   }
-
-  for(let a = 0; a < candidateAnswers.length; a++){
-  if (candidateAnswers[a].toLowerCase().trim() === correctAnswers[a].toLowerCase().trim()){
-    score = score + 1; 
-  } else{
-  
-  }
-} return score
 }
-
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let score = 0
 
-  let grade =  score/question * 100
-  console.log(`You answered ${score}/5 questions correct, that's a ${grade}%`)
+ for(let a = 0; a < candidateAnswers.length; a++){
+  if (candidateAnswers[a].toLowerCase() === correctAnswers[a].toLowerCase()){
+    score = score + 1; 
+  }
+ }
+
+  let grade = (score/5) * 100
+  console.log(`>>> Overall Grade: ${grade}% (${score} of 5 responses correct) <<<`)
   if (grade >= 80){
-    console.log("Status: PASSED")
+    console.log(">>> Status: PASSED<<<")
   } else{
-    console.log("Status: FAILED")
+    console.log(">>> Status: FAILED<<<")
   }
   return grade
 }
