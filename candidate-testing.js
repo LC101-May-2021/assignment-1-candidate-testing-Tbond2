c9a31499dfae22dfe8764c52c0b101f7392c6fc5
Tyler Bond <>
2021-06-05 02:19:08 +0000

    Autograder I hate you

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,7 @@ let candidateAnswer = '';
 let questions = ["Who was the first American woman in space?","True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit','Trajectory', 45] what entry is at index 2?","What is the minimum crew size for the International Space Station (ISS)?"]
 let correctAnswers = ['Sally Ride', "True", "40", "Trajectory", "3"]
 let candidateAnswers = []
-let candidateScore = 0
+let score = 0
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -27,19 +27,19 @@ function askQuestion() {
 
   for(let a = 0; a < candidateAnswers.length; a++){
   if (candidateAnswers[a].toLowerCase().trim() === correctAnswers[a].toLowerCase().trim()){
-    candidateScore = candidateScore + 1; 
+    score = score + 1; 
   } else{
   
   }
-} return candidateScore
+} return score
 }
 
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let grade =  candidateScore/question * 100
-  console.log(`You answered ${candidateScore}/5 questions correct, that's a ${grade}%`)
+  let grade =  score/question * 100
+  console.log(`You answered ${score}/5 questions correct, that's a ${grade}%`)
   if (grade >= 80){
     console.log("Status: PASSED")
   } else{
